3540dcb25cfb6fc8d173a6486c335d1a7332ade3
drm121918-dev <drm121918@gmail.com>
2025-11-23 19:20:33 +0530

    Update API credentials and owner ID in config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = "29978901"
+API_ID = API_ID = "36925285"
 
-API_HASH = os.environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
+API_HASH = os.environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8464714544:AAF7cN9yIE_JoLap0HsjXlEhuGCKspr-jtw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8361069721:AAENqJs0H83lUP4LSM0mFX1Mx3jEtCEQ4zA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 5776977809))
+OWNER = int(os.environ.get("OWNER", 6090172625))
 
-LOG = -1003068385283
+
 
